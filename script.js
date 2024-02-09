faf1bfa8d94230b4736d6d4d6c10454e2ccff865
Yanz0817 <156667199+Yanz0817@users.noreply.github.com>
2024-02-09 23:13:39 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+
+function calculate(){
+
+  var v1 = parseInt(document.getElementById('input1').value);
+  var v2 = parseInt(document.getElementById('input2').value);
+  var operator = document.getElementById('operation').value;
+  
+  var result;
+
+   
+      if(operator === "Sum"){
+       result=v1+v2;
+    
+    } 
+    
+    else if(operator === "Minus"){
+      result=v1-v2;
+      
+    }
+    else if(operator === "Multiply"){
+       result=v1*v2;
+      
+    } 
+    
+    else if(operator === "Divide"){
+      result=v1/v2;
+       
+    } 
+      else{
+      alert('Please put number and operator');
+      return;
+    } 
+    
+   alert('the answer is' + " " + result);
+   
+}
+
+
+
